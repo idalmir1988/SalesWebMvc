9e4b51354c309fb3b27baf3fdd4bd95fefed76ad
idalmir1988 <idalmir.oliveira@hotmail.com>
2021-10-22 17:09:09 -0300

    View Models

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -42,6 +42,7 @@ namespace SalesWebMvc
 
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
+            services.AddScoped<DepartmentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +51,7 @@ namespace SalesWebMvc
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                seedingService.Seed();
+                //seedingService.Seed();
             }
             else
             {
